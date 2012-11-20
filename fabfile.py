7e03f5794c8baf316f46051b213dab6ba3ff58d2
Antoine Martin <antoine@openance.com>
2012-11-20 19:52:14 +0100

    Initial commit

---
+++ fabfile.py
@@ -0,0 +1,336 @@
+import config
+import boto
+import boto.ec2
+from fabric.colors import green, red, yellow
+from fabric.api import *
+from fabric.context_managers import cd
+import time
+from StringIO import StringIO
+import StringIO
+
+# Constants
+VOLUME_SIZE = getattr(config, 'VOLUME_SIZE', 15)
+MAIN_PARTITION_SIZE = getattr(config, 'MAIN_PARTITION_SIZE', VOLUME_SIZE-2)
+SWAP_PARTITION_SIZE = VOLUME_SIZE-MAIN_PARTITION_SIZE
+MAIN_PARTITION_MOUNT_POINT = getattr(config, 'MAIN_PARTITION_MOUNT_POINT', '/mnt/archec2build')
+PACKAGES = getattr(config, 'PACKAGES', 'base base-devel openssh polkit sudo')
+HOSTNAME = getattr(config, 'HOSTNAME', 'archec2')
+LANG = getattr(config, 'LANG', 'fr_FR.UTF-8')
+KEYMAP = getattr(config, 'KEYMAP', 'fr')
+TIMEZONE = getattr(config, 'TIMEZONE', 'Europe/Paris')
+
+FDISK_INPUT="""n
+p
+
+
++%dG
+n
+p
+
+
+
+t
+2
+82
+w
+""" % MAIN_PARTITION_SIZE
+
+FDISK_DELETE_INPUT="""d
+1
+d
+w
+"""
+
+MINIMAL_PACMAN_CONF="""
+[options]
+HoldPkg     = pacman glibc
+SyncFirst   = pacman
+Architecture = %(arch)s
+#[ec2]
+#Server = file://${BASEDIR}/repo
+[core]
+Include = /etc/pacman.d/mirrorlist
+[extra]
+Include = /etc/pacman.d/mirrorlist
+[community]
+Include = /etc/pacman.d/mirrorlist
+"""
+
+GRUB_MENU_LST="""
+default 0
+timeout 1
+
+title  Arch Linux
+    root   (hd0,0)
+    kernel /boot/vmlinuz-linux-ec2 root=/dev/xvda1 ip=dhcp console=hvc0 spinlock=tickless ro init=/bin/systemd
+    initrd /boot/initramfs-linux-ec2.img
+"""
+
+FSTAB_TEMPLATE="""
+tmpfs /tmp tmpfs nodev,nosuid    0       0
+UUID=%(main_id)s / auto defaults,relatime,data=ordered 0 2
+UUID=%(swap_id)s none swap defaults 0 0
+"""
+
+
+def create_ec2_connection(region=config.EC2_REGION):
+    return boto.ec2.connect_to_region(config.EC2_REGION, aws_access_key_id=config.AWS_ACCESS_KEY_ID, aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY)
+
+def get_instance(connection=create_ec2_connection, instance_id=config.EC2_BUILD_INSTANCE):
+    if callable(connection):
+        connection = connection()
+    reservation = connection.get_all_instances(instance_ids=(instance_id,))[0]
+    if not reservation:
+        raise Exception('No instance with name %s' % instance_id)
+    return reservation.instances[0]
+
+    
+def get_hostname_from_instance(connection, instance_id=config.EC2_BUILD_INSTANCE):
+    return get_instance(connection, instance_id).dns_name
+    
+    
+# Steps to produce the AMI
+#
+# - Check that the build environment is ok -> OK
+# - Create the EBS volume -> OK
+# - Attach the EBS volume to the build machine -> OK
+# - Build partitions on the volume -> OK
+# - Format the partitions on the volume -> OK
+# - Mount the main partition -> OK
+# - launch pacstrap -> OK
+# - Create configuration files
+#   - hostname -> OK
+#   - locale -> OK
+#   - timezone -> OK
+#   - time format ->OK
+#   - vconsole keymap -> OK
+#   - fstab -> OK
+#   - ssh config -> OK
+#   - root account -> OK
+# - Enable systemd services
+#   - sshd -> OK
+#   - cron
+#   - ec2
+# - Create grub file -> OK
+# - Unmout the filesystem
+# - un-attach the volume
+# - create a snapshot
+# - create the AMI from the snapshot
+# - Launch an instance from the snapshot
+
+env.hosts = [ 'root@%s' % get_hostname_from_instance(create_ec2_connection())]
+
+def install_packages(*args):
+    with hide('output'):
+        run('pacman -Sy --noconfirm --needed %s' % ' '.join(args) )
+        
+def remove_packages(*args):
+    with hide('output'):
+        run('pacman -Rc --noconfirm %s' % ' '.join(args) )
+        
+
+@task()
+def check_install_scripts():
+    out = run('pacman -Qi arch-install-scripts', quiet=True)
+    if out.failed:
+        print yellow("Arch install scripts is not installed. Installing...")
+        install_packages('arch-install-scripts')
+    else:
+        print green("Arch install scripts is installed")
+        
+        
+@task()
+def clean_env():
+    remove_packages('arch-install-scripts')
+    
+    
+def find_free_device(instance):
+    letter = 'h'
+    mapping = instance.block_device_mapping
+    value = '/dev/sd%s' % letter
+    while mapping.has_key(value):
+        letter = chr(ord(letter) + 1)
+        value = '/dev/sd%s' % letter
+    return value
+
+def find_build_device(instance):
+    for mount_point, device in instance.block_device_mapping.iteritems():
+        if not device.delete_on_termination:
+            volume =  instance.connection.get_all_volumes((device.volume_id,))[0]
+            if volume.tags.has_key('archec2build'):
+                return (volume, mount_point.replace('/sd', '/xvd'))
+    return (None,None)
+
+def find_snapshots(connection=create_ec2_connection, name='archec2build'):
+    if callable(connection):
+        connection=connection()
+    return connection.get_all_snapshots(filters={'tag:Name' : name})
+
+USE_SNAPSHOT = getattr(config, 'USE_SNAPSHOT', False)
+SNAPSHOT_ID = getattr(config, 'SNAPSHOT_ID', find_snapshots()[0] if USE_SNAPSHOT else None)
+
+@task
+def create_and_attach_volume():
+    connection = create_ec2_connection()
+    instance = get_instance(connection)
+    print green('Attach volume using snapshot %s' % SNAPSHOT_ID)
+    vol = connection.create_volume(VOLUME_SIZE, instance.placement, snapshot=SNAPSHOT_ID)
+    vol.add_tag('archec2build', '')
+    mount_point = find_free_device(instance)
+    vol.attach(instance.id, mount_point)
+    status = vol.update()
+    while status != 'in-use':
+        time.sleep(5)
+        status = vol.update()
+    return (vol, mount_point)
+
+def get_volume():
+    instance = get_instance()
+    volume, device_name = find_build_device(instance)
+    return (instance, volume, device_name)
+    
+@task
+def decomission_volume():
+    connection = create_ec2_connection()
+    instance = get_instance(connection)
+    volume, mount_point = find_build_device(instance)
+    if not volume:
+        print red("Could not find build volume")
+    else:
+        print green("Found build volume at device %s" % mount_point)
+        volume.detach()
+        status = volume.update()
+        while status != 'available':
+            time.sleep(5)
+            status = volume.update()
+        volume.delete()        
+    
+    
+@task
+def create_volume_partitions(input_string=FDISK_INPUT):
+    instance = get_instance()
+    volume, mount_point = find_build_device(instance)
+    if not volume:
+        print red("Could not find build volume")
+    else:
+        print green("Found build volume at device %s" % mount_point)
+        with cd('/tmp'):
+            input_filename = 'archec2build_fdisk_input'
+            put(StringIO(input_string), input_filename)
+            run('fdisk %s < %s' % (mount_point, input_filename))
+            run('rm %s' % input_filename)
+
+@task
+def delete_volume_partitions():
+    create_volume_partitions(FDISK_DELETE_INPUT)
+            
+@task
+def format_volume_partitions():
+    instance, volume, device_name = get_volume()
+    run("mkfs.ext4 -L ac2root %s1" % device_name)
+    run("mkswap -L ac2swap %s2" % device_name)
+
+def mount_main_partition(device_name):
+    run('mkdir -p %s' % MAIN_PARTITION_MOUNT_POINT)
+    run('mount %s1 %s' % (device_name, MAIN_PARTITION_MOUNT_POINT))
+                   
+def unmount_main_partition():
+    run('umount %s' % MAIN_PARTITION_MOUNT_POINT)
+    run('rm -rf %s' % MAIN_PARTITION_MOUNT_POINT)
+                
+@task
+def create_volume_snapshot():
+    instance, volume, device_name = get_volume()
+    snapshot = volume.connection.create_snapshot(volume.id, 'archec2build')
+    if snapshot:    
+        print green('Snapshot %s created' % snapshot.id)
+        status = snapshot.update()
+        while status == 'pending':
+            time.sleep(3)
+            status = snapshot.update()
+        snapshot.add_tag('Name', 'archec2build')
+        snapshot.add_tag('archec2build', '')
+    else:
+        print red('Snapshot not created')
+    
+@task 
+def bootstrap_archlinux():
+    instance, volume, device_name = get_volume()
+    mount_main_partition(device_name)
+    arch = run('uname -m')    
+    pacman_filename = '/tmp/archec2build_pacman.conf'
+    put(StringIO(MINIMAL_PACMAN_CONF % { 'arch' : arch}), pacman_filename )
+    with hide('output'):    
+        run('pacstrap -C %s %s %s' % (pacman_filename, MAIN_PARTITION_MOUNT_POINT, PACKAGES))
+    unmount_main_partition()
+    run('rm -rf %s' % pacman_filename)
+    
+@task
+def configure_archlinux():
+    instance, volume, device_name = get_volume()
+
+    mount_main_partition(device_name)
+    
+    # hostname
+    run('echo %s > %s/etc/hostname' % (HOSTNAME, MAIN_PARTITION_MOUNT_POINT))
+    # timezone
+    run('arch-chroot %s ln -s /usr/share/zoneinfo/%s /etc/localtime' % ( MAIN_PARTITION_MOUNT_POINT, TIMEZONE))
+    # lang & keymap
+    run('echo "LANG=%s" > %s/etc/locale.conf' % (LANG, MAIN_PARTITION_MOUNT_POINT))
+    run('echo "KEYMAP=%s" > %s/etc/vconsole.conf' % (KEYMAP, MAIN_PARTITION_MOUNT_POINT))
+    
+    # generate locale
+    locale_gen_filename = '%s/etc/locale.gen' % MAIN_PARTITION_MOUNT_POINT
+    run('mv %(path)s %(path)s.orig' % { 'path' : locale_gen_filename })
+    lang_string = "en_US.UTF-8 UTF-8\n"
+    if LANG != 'en_US.UTF-8':
+        lang_string = '\n'.join([ '%s UTF-8' % LANG, lang_string])
+   
+    put(StringIO(lang_string), locale_gen_filename)
+    run('arch-chroot %s locale-gen' % MAIN_PARTITION_MOUNT_POINT)
+    run('arch-chroot %s systemctl enable sshd.service' % MAIN_PARTITION_MOUNT_POINT)
+    run('arch-chroot %s systemctl enable cronie.service' % MAIN_PARTITION_MOUNT_POINT)
+    run('arch-chroot %s systemctl enable ec2.service' % MAIN_PARTITION_MOUNT_POINT)
+    run('arch-chroot %s hwclock --systohc --utc' % MAIN_PARTITION_MOUNT_POINT)
+    
+    # menu.lst
+    run('mkdir -p %s/boot/grub' % MAIN_PARTITION_MOUNT_POINT)
+    put(StringIO(GRUB_MENU_LST), '%s/boot/grub/menu.lst' % MAIN_PARTITION_MOUNT_POINT)
+
+    # ssh configuration
+    sshd_config_filename = '%s/etc/ssh/sshd_config' % MAIN_PARTITION_MOUNT_POINT
+    run('cp %(path)s %(path)s.orig' % { 'path' : sshd_config_filename })
+    run("sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' %s" % sshd_config_filename)
+    run("sed -i 's/#UseDNS yes/UseDNS no/' %s" % sshd_config_filename)
+    
+    # basic root creation
+    root_dir = '%s/root' % MAIN_PARTITION_MOUNT_POINT
+    run('cp %s/etc/skel/.bash* %s' % (MAIN_PARTITION_MOUNT_POINT, root_dir))
+    run('touch %s/firstboot' % root_dir)
+    
+    # sudo configuration
+    sudo_file = '%s/etc/sudoers.d/wheel' % MAIN_PARTITION_MOUNT_POINT
+    put(StringIO("%wheel ALL=(ALL) NOPASSWD: ALL"), sudo_file)
+    run('chmod 440 %s' % sudo_file) 
+    
+    # fstab
+    fstab = '%s/etc/fstab' % MAIN_PARTITION_MOUNT_POINT
+    run('mv %(path)s %(path)s.orig' % { 'path' : fstab})
+    main_partition_id = run('blkid -c /dev/null -s UUID -o value %s1' % device_name)
+    swap_partition_id = run('blkid -c /dev/null -s UUID -o value %s2' % device_name)
+    fstab_content = FSTAB_TEMPLATE % { 'main_id' : main_partition_id, 'swap_id' : swap_partition_id}
+    put(StringIO(fstab_content), fstab)
+    
+    # nameserver
+    resolv = '%s/etc/resolv.conf' % MAIN_PARTITION_MOUNT_POINT
+    run('mv %(path)s %(path)s.orig' % { 'path' : resolv})
+    put(StringIO("nameserver 172.16.0.23"), resolv)
+
+    # pacman.conf    
+    arch = run('uname -m')    
+    pacman_filename = '%s/etc/pacman.conf' % MAIN_PARTITION_MOUNT_POINT
+    put(StringIO(MINIMAL_PACMAN_CONF % { 'arch' : arch}), pacman_filename )
+    
+    unmount_main_partition()
+    
+    
