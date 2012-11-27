8b37799750eb72c78c2330d4999483b2039613dc
Antoine Martin <antoine@openance.com>
2012-11-27 21:17:22 +0100

    Build a complete image from scratch, based on the previous image.

--- fabfile.py
+++ fabfile.py
@@ -3,14 +3,21 @@ import config
 import boto
 import boto.ec2
 from boto.ec2.blockdevicemapping import EBSBlockDeviceType, BlockDeviceMapping 
-from fabric.colors import green, red, yellow, white
+from fabric.colors import green, red, yellow, white, blue
 from fabric.api import *
 from fabric.context_managers import cd
 import time
 import datetime
 from StringIO import StringIO
 
-# Constants
+
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+##############
+# CONSTANTS
+##############
+
 VOLUME_SIZE = getattr(config, 'VOLUME_SIZE', 15)
 MAIN_PARTITION_SIZE = getattr(config, 'MAIN_PARTITION_SIZE', VOLUME_SIZE-2)
 SWAP_PARTITION_SIZE = VOLUME_SIZE-MAIN_PARTITION_SIZE
@@ -23,8 +30,11 @@ PACKAGES_FILENAME = getattr(config, 'PACKAGES_FILENAME', 'packages')
 IMAGE_DESCRIPTION = getattr(config, 'IMAGE_DESCRIPTION', 'ArchLinux EC2 Image')
 INSTANCE_KEY_NAME = getattr(config, 'INSTANCE_KEY_NAME', 'default.eu')
 INSTANCE_SECURITY_GROUP = getattr(config, 'INSTANCE_SECURITY_GROUP', 'default')
+EC2_BUILD_INSTANCE = getattr(config, 'EC2_BUILD_INSTANCE', 'Unknown')
 
-
+##################
+# TEMPLATES
+##################
 FDISK_INPUT_SAV="""n
 p
 
@@ -87,30 +97,54 @@ UUID=%(main_id)s / auto defaults,relatime,data=ordered 0 2
 """
 #UUID=%(swap_id)s none swap defaults 0 0
 
+MIRRORLIST="""
+Server = http://mirror.i3d.net/pub/archlinux/$repo/os/$arch
+Server = http://archlinux.mirrors.ovh.net/archlinux/$repo/os/$arch
+Server = http://fruk.org/archlinux/$repo/os/$arch
+Server = http://mirror.bytemark.co.uk/archlinux/$repo/os/$arch
+Server = http://arch.apt-get.eu/$repo/os/$arch
+Server = http://mir.archlinux.fr/$repo/os/$arch
+Server = http://ftp.iinet.net.au/pub/archlinux/$repo/os/$arch
+Server = http://archlinux.supsec.org/$repo/os/$arch
+"""
+
 def create_ec2_connection(region=config.EC2_REGION):
     return boto.ec2.connect_to_region(config.EC2_REGION, aws_access_key_id=config.AWS_ACCESS_KEY_ID, aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY)
 
-def get_instance(connection=create_ec2_connection, instance_id=config.EC2_BUILD_INSTANCE):
+def get_instance(connection=create_ec2_connection, instance_id=None):
     if callable(connection):
         connection = connection()
+    instance_id = instance_id or EC2_BUILD_INSTANCE
     reservation = connection.get_all_instances(instance_ids=(instance_id,))[0]
     if not reservation:
         raise Exception('No instance with name %s' % instance_id)
     return reservation.instances[0]
 
     
-def get_hostname_from_instance(connection=create_ec2_connection, instance_id=config.EC2_BUILD_INSTANCE):
+def get_hostname_from_instance(connection=create_ec2_connection, instance_id=None):
+    instance_id = instance_id or EC2_BUILD_INSTANCE
     return get_instance(connection, instance_id).dns_name
     
 env.user = 'root'
-env.hosts = [ '%s' % get_hostname_from_instance()]
+env.hosts = [ get_hostname_from_instance()]
+
+SNAPSHOT_NAME_TEMPLATE = '%s.build.%s'
+IMAGE_NAME_TEMPLATE = '%s.image.%s'
+INSTANCE_NAME_TEMPLATE = '%s.instance.%s'
 
 ARCH = getattr(config, 'ARCH', get_instance().architecture)
-PREFIX = getattr(config, 'PREFIX', 'archec2.%s' % datetime.datetime.today().strftime('%Y%m%d'))
-IMAGE_NAME = getattr(config, 'IMAGE_NAME', '%s.image.%s' % (PREFIX, ARCH))
-SNAPSHOT_NAME = getattr(config, 'SNAPSHOT_NAME', '%s.build.%s' % (PREFIX, ARCH))
+BASE_PREFIX = getattr(config, 'BASE_PREFIX', 'archec2')
+DATE_STRING = getattr(config, 'DATE_STRING', datetime.datetime.today().strftime('%Y%m%d'))
+PREFIX = getattr(config, 'PREFIX', '%s.%s' % (BASE_PREFIX, DATE_STRING))
+
+IMAGE_NAME = getattr(config, 'IMAGE_NAME', IMAGE_NAME_TEMPLATE % (PREFIX, ARCH))
+SNAPSHOT_NAME = getattr(config, 'SNAPSHOT_NAME', SNAPSHOT_NAME_TEMPLATE % (PREFIX, ARCH))
 VOLUME_NAME = getattr(config, 'VOLUME_NAME', SNAPSHOT_NAME)
-INSTANCE_NAME = getattr(config, 'INSTANCE_NAME', '%s.instance.%s' % (PREFIX, ARCH))
+INSTANCE_NAME = getattr(config, 'INSTANCE_NAME', INSTANCE_NAME_TEMPLATE % (PREFIX, ARCH))
+
+BASE_IMAGE_NAME = getattr(config, 'IMAGE_NAME', IMAGE_NAME_TEMPLATE % (BASE_PREFIX, ARCH))
+BASE_SNAPSHOT_NAME = getattr(config, 'SNAPSHOT_NAME', SNAPSHOT_NAME_TEMPLATE % (BASE_PREFIX, ARCH))
+BASE_INSTANCE_NAME = getattr(config, 'INSTANCE_NAME', INSTANCE_NAME_TEMPLATE % (BASE_PREFIX, ARCH))
 
 
 def install_packages(*args):
@@ -124,6 +158,13 @@ def remove_packages(*args):
 
 @task()
 def check_install_scripts():
+    out = run('test -h /etc/pacman.d/mirrorlist', quiet=True)
+    if out.succeeded:
+        print yellow('mirrorlist link bug. Patching...')
+        run('rm /etc/pacman.d/mirrorlist')
+        put(StringIO(MIRRORLIST), '/etc/pacman.d/mirrorlist.backup')
+        run('rankmirrors -n 3 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist')
+        run('pacman -Syy')
     out = run('pacman -Qi arch-install-scripts', quiet=True)
     if out.failed:
         print yellow("Arch install scripts is not installed. Installing...")
@@ -131,12 +172,13 @@ def check_install_scripts():
     else:
         print green("Arch install scripts is installed")
         
-        
 @task()
 def clean_env():
     remove_packages('arch-install-scripts')
     
-    
+#
+# Find methods
+#    
 def find_free_device(instance):
     letter = 'h'
     mapping = instance.block_device_mapping
@@ -157,19 +199,46 @@ def find_build_device(instance):
 def find_snapshots(connection=create_ec2_connection, name=SNAPSHOT_NAME):
     if callable(connection):
         connection=connection()
-    return connection.get_all_snapshots(filters={'tag:Name' : name})
+    result = connection.get_all_snapshots(filters={'tag:%s' % name : ''})
+    if not result or len(result) == 0:
+        result =  connection.get_all_snapshots(filters={'tag:Name' : name})
+    return result
+
+def find_images(connection=create_ec2_connection, name=IMAGE_NAME):
+    if callable(connection):
+        connection=connection()
+    result = connection.get_all_images(filters={'tag:%s' % name : ''})
+    if not result or len(result) == 0:
+        result =  connection.get_all_images(filters={'tag:Name' : name})
+    return result
+
+def find_instances(connection=create_ec2_connection, name=INSTANCE_NAME):
+    if callable(connection):
+        connection=connection()
+    result = connection.get_all_instances(filters={'tag:%s' % name : ''})
+    if not result or len(result) == 0:
+        result =  connection.get_all_instances(filters={'tag:Name' : name})
+    return [res.instances[0] for res in result]
+
 
 def delete_snapshots(name=SNAPSHOT_NAME):
     for snapshot in find_snapshots(name=name):
         print green('Deleting snapshot with id %s' % snapshot.id)
         snapshot.delete()
+        
+def add_name(obj, name):
+    obj.add_tag(name, '')
+    obj.add_tag('Name', name)
+    
 
 @task
 def delete_build_snapshots():
+    "Deletes the snapshots taken after the build."
     delete_snapshots(name=SNAPSHOT_NAME)
 
 @task
 def delete_image_snapshots():
+    "Deletes the snapshot from which the image is derived."
     delete_snapshots(name=IMAGE_NAME)
 
 USE_SNAPSHOT = getattr(config, 'USE_SNAPSHOT', False)
@@ -177,12 +246,17 @@ SNAPSHOT_ID = getattr(config, 'SNAPSHOT_ID', find_snapshots()[0] if USE_SNAPSHOT
 
 @task
 def create_and_attach_volume():
+    """Creates the build volume an attaches it to the build instance.
+    
+    If a snapshot id is specified or USE_SNAPSHOT is True, 
+    instead of building a new volume,it creates the volume 
+    from the snapshot.
+    """
     connection = create_ec2_connection()
     instance = get_instance(connection)
     print green('Attach volume using snapshot %s' % SNAPSHOT_ID)
     vol = connection.create_volume(VOLUME_SIZE, instance.placement, snapshot=SNAPSHOT_ID)
-    vol.add_tag(SNAPSHOT_NAME, '')
-    vol.add_tag('Name', SNAPSHOT_NAME)
+    add_name(vol, SNAPSHOT_NAME)
     mount_point = find_free_device(instance)
     vol.attach(instance.id, mount_point)
     status = vol.update()
@@ -255,8 +329,7 @@ def create_snapshot(name=SNAPSHOT_NAME):
         while status != '100%':
             time.sleep(3)
             status = snapshot.update()
-        snapshot.add_tag('Name', name)
-        snapshot.add_tag(name, '')
+        add_name(snapshot, name)
         return snapshot
     else:
         print red('Snapshot not created')
@@ -282,7 +355,7 @@ def bootstrap_archlinux():
         else:    
             run('pacstrap -C %s %s %s' % (pacman_filename, MAIN_PARTITION_MOUNT_POINT, get_packages()))
     unmount_main_partition()
-    run('rm -rf %s' % pacman_filename)
+    run('rm -rf %s' % pacman_filename)    
     
 @task
 def configure_archlinux():
@@ -361,8 +434,7 @@ def create_image(name=IMAGE_NAME, description=IMAGE_DESCRIPTION):
         print red('Cannot create image with no snapshot')
     else:
         # Create block device mapping
-        ebs = EBSBlockDeviceType() 
-        ebs.snapshot_id = snapshot.id 
+        ebs = EBSBlockDeviceType(snapshot_id=snapshot.id, delete_on_termination=True) 
         block_map = BlockDeviceMapping() 
         block_map['/dev/sda'] = ebs 
 
@@ -384,13 +456,7 @@ def create_image(name=IMAGE_NAME, description=IMAGE_DESCRIPTION):
         print green('Image id is %s' % image_id)
         time.sleep(3)
         image = instance.connection.get_all_images((image_id,))[0]
-        image.add_tag('Name', name)
-        image.add_tag(name, '')
-
-def find_images(connection=create_ec2_connection, name=IMAGE_NAME):
-    if callable(connection):
-        connection=connection()
-    return connection.get_all_images(filters={'tag:Name' : name})
+        add_name(image, name)
 
 @task
 def delete_images(name=IMAGE_NAME):
@@ -420,43 +486,93 @@ def launch_instance(image_name=IMAGE_NAME, instance_name=INSTANCE_NAME):
                 print white('Waiting...') 
                 time.sleep(3)
                 status = instance.update()
-            instance.add_tag(instance_name, '')
-            instance.add_tag('Name', instance_name)
+            add_name(instance, instance_name)
             print green('Instance %s with dns_name %s launched' % (instance.id, instance.dns_name))
     return instance
 
-def find_instances(connection=create_ec2_connection, tag=INSTANCE_NAME):
-    if callable(connection):
-        connection=connection()
-    return [res.instances[0] for res in  connection.get_all_instances(filters={'tag:%s' % tag: ''})]
 
 @task
-def delete_instances(tag=INSTANCE_NAME):
-    for instance in find_instances(tag=tag):
+def delete_instances(name=INSTANCE_NAME):
+    for instance in find_instances(name=name):
         if instance.update() == 'running':
             print green('Deleting running instance with id %s and dns_name %s' % (instance.id, instance.dns_name))
             instance.terminate()
             
 @task
-def make_image():
+def make_image(create_snapshot=False):
     check_install_scripts()
     create_and_attach_volume()
     time.sleep(10)
     create_volume_partitions()
     format_volume_partitions()
     bootstrap_archlinux()
-    create_volume_snapshot()
+    if create_snapshot:
+        create_volume_snapshot()
     configure_archlinux()
     create_image()
+    
+    
+def check_instance(instance):
+    with settings(
+        hide('output'),
+        host_string='root@%s' % instance.dns_name, 
+        warn_only=True,
+        connection_attempts=5):
+        print green('Waiting for server to answser...')
+        run('uname -a')    
     
 @task 
 def launch_instance_and_wait(image_name=IMAGE_NAME, instance_name=INSTANCE_NAME):
     instance = launch_instance(image_name, instance_name)
-    with settings(
-        hide('output'),
-        host_string='%s' % get_hostname_from_instance(instance_id=instance.id), 
-        warn_only=True,
-        connection_attempts=5):
-        print green('Waiting for server to answser...')
-        run('uname -a')
+    check_instance(instance)
+    return instance
+
+@task
+def check_connectivity(name=INSTANCE_NAME):
+    instances = filter(lambda x : x.update() == 'running', find_instances(name=name))
+    for instance in instances:
+        check_instance(instance) 
+
+
+def change_base(find_method, base_name, new_name=None):
+    old = find_method(name=base_name)[0]
+    _new = find_method(name=new_name)[0] if new_name else find_method()[0]  
+    old.remove_tag(base_name)
+    _new.add_tag(base_name, '')
+    
+@task(default=True)
+def build_all():
+    """This is the default task.
+    
+    This task does the following:
+    - Launches an instance of the current _working_ image.
+    - Build a new new image on this instance.
+    - Launches an instance with the new image to check that the image works.
+    - Sets the newly build image as the base image.
+    """
+
+    existing_running_instances = filter(lambda x : x.update() == 'running', find_instances(name=BASE_INSTANCE_NAME))
+    if existing_running_instances and len(existing_running_instances) > 0:
+        build_instance = existing_running_instances[0]
+        check_instance(build_instance)
+        print blue('Using existing instance %s...' % build_instance.id)
+    else:     
+        print blue('Launching build instance...')
+        build_instance = launch_instance_and_wait(BASE_IMAGE_NAME, BASE_INSTANCE_NAME)
+    global EC2_BUILD_INSTANCE
+    EC2_BUILD_INSTANCE = build_instance.id
+    with settings(host_string='root@%s' % build_instance.dns_name):
+        print blue('Building image...')
+        make_image()
+        print blue('Checking that image works...')
+        new_instance = launch_instance_and_wait()
+        new_instance.terminate()
+        print blue('Cleaning build workspace...')
+        decomission_volume()
+        build_instance.terminate()
+        print blue('Promoting new image to base image')
+        change_base(find_images, BASE_IMAGE_NAME)
+        change_base(find_snapshots, BASE_IMAGE_NAME, IMAGE_NAME)
         
+    
+    
